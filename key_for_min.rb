315a67a5e722b5d287be08c9fa2a205c8753deac
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-28 01:22:25 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     nil
   else
-  lowest_key = name_hash.sort_by { |key, value| value}.first
+  lowest_key = name_hash.sort_by {|key, value| value}.first
   lowest_key.first
   end
 end
