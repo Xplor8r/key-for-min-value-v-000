58e477de39a4fac1f663c4d349dcf23874d5c596
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-28 00:59:36 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.sort_by do |key, value| 
-    value
-  end
-  return value.first
+  lowest_key = name_hash.sort_by { |key, value| value}
+  lowest_key.first
 end
